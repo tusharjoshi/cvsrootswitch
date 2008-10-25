3508309433722d8da4e48b64e1340ac2884b3ece
tusharvjoshi <tusharvjoshi@b8c34e0e-94b8-11dd-b98f-65027bbbf987>
2008-10-25 13:39:54 +0000

    Implemented GUI for the program. Added GUI component library

--- trunk/src/com/infospectrum/scm/tools/cvs/RootChangerShell.java
+++ trunk/src/com/infospectrum/scm/tools/cvs/RootChangerShell.java
@@ -4,7 +4,14 @@
  */
 package com.infospectrum.scm.tools.cvs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
+ * Provides a command line interface.
  *
  * @author <a href="tusharvjoshi@gmail.com">Tushar Joshi</a>
  */
@@ -15,9 +22,13 @@ public class RootChangerShell {
      */
     public static void main(String[] args) {
 
+        /**
+         * If we do not have required parameters show
+         * message and do nothing.
+         */
         if (args.length != 3) {
             System.err.println("Pass arguments as follows:");
-            System.err.println("pathtofolder oldroot newroot");
+            System.err.println("path-of-folder old-root new-root");
             return;
         }
 
@@ -25,6 +36,38 @@ public class RootChangerShell {
         final String oldRoot = args[1];
         final String newRoot = args[2];
 
+        /*
+         * Confirm from the user about his inputs
+         * and provide a way to cancel the transaction
+         */
+        System.out.println("I will start from the folder: " + path);
+        System.out.println("I will search for CVS Root files and replace,");
+        System.out.println("all occurences of \"" + oldRoot + "\" with ");
+        System.out.println("new value \"" + newRoot + "\"");
+        System.out.println("");
+        System.out.print("Are you sure (y/n)?");
+
+        try {
+            InputStreamReader reader = new InputStreamReader(System.in);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line = bufferedReader.readLine();
+            if (line != null && line.equalsIgnoreCase("Y")) {
+                /**
+                 * we got the users confirmation hence proceed
+                 * and change the roots.
+                 */
+                System.out.println("Starting root switch process...");
+                changeRoots(path, oldRoot, newRoot);
+                System.out.println("Root switch succesfully done!");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(RootChangerShell.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+        }
+
+    }
+
+    private static void changeRoots(String path, String oldRoot, String newRoot) {
         final RootChanger rootChanger = new RootChanger(path, oldRoot, newRoot);
         rootChanger.changeRoots(new RootChangeAdapter() {
 
@@ -34,6 +77,5 @@ public class RootChangerShell {
 
             }
         });
-
     }
 }
